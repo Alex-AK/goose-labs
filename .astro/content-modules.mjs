418aaf15f90083a92608e7b589bf1b8c7f126bc5
alex <hello@alex-ak.com>
2025-07-24 17:11:16 -0700

    Add ip-change-detection, sort tags, remove .astro files

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/project/discord-voice-notification-bot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fdiscord-voice-notification-bot.mdx&astroContentModuleFlag=true")],
-["src/content/project/self-hosted-n8n.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fself-hosted-n8n.mdx&astroContentModuleFlag=true")],
-["src/content/project/home-server.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproject%2Fhome-server.mdx&astroContentModuleFlag=true")]]);
-		
